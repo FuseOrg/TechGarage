80aefc41117d5bd80f044b2b495329dbbd600566
AjithPG <ajithpg008@gmail.com>
2021-08-07 23:19:59 +0530

    Initial Upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+
+
+    <div className="bg-gray-900 p-20 h-screen flex justify-center items-center items-start flex-col">
+       <h1 className="text-5xl text-white">Welcome to 🛠️Techgarage 👋</h1>
+       <p className="text-gray-400 mt-5 text-lg">Tech garage is a Platform to access different tools available on the internet</p>
+        <button className="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+          Hello Friends 🚀
+        </button>
+    </div>
+  )
+
+}
+
+export default App;
