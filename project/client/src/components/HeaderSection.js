f53f423ada741d942ce6643a73ae9a76e9707db7
AjithPG <ajithpg008@gmail.com>
2021-09-04 10:24:01 +0530

    Search bar color fix

--- project/client/src/components/HeaderSection.js
+++ project/client/src/components/HeaderSection.js
@@ -3,13 +3,14 @@ import './HeaderSection.css'
 const HeaderSection = (props) => {
     return (
       <header className="w-full bg-secondary py-4 mt-14">
-         <div className="header_container flex flex-col justify-center items-center w-11/12 mx-auto text-white">
+         <div className="header_container flex flex-col justify-center items-center w-11/12 mx-auto ">
              <div className="image_conatiner">
                  <img src={logo} alt="logo"/>
              </div>
              <div className="header_description flex flex-col text-center">
-                 <h1 className="text-2xl mb-1">Free Resources for Developers</h1>
-                 <p className="text-md mb-4"> This is  a great place to start exploring  resources that are free to use.</p>
+                 <h1 className="text-2xl mb-1 text-white">Free Resources for Developers</h1>
+                 <p className="text-md mb-4 text-white"> This is  a great place to start exploring  resources that are free to use.</p>
+                 
                  <input type="text" placeholder="Search" className="p-2 rounded-lg text-sm mb-4"/>
              </div>
          </div>
