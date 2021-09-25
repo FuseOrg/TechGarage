17c76cae6cb349ac20e709f0c2be58ec2c490ac8
AjithPG <ajithpg008@gmail.com>
2021-09-25 20:29:25 +0530

    Added new boxshadow and new images for icons

--- project/client/tailwind.config.js
+++ project/client/tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'secondary':'#2B2F33',
         'secondary-light': '#858B8D',
         'third-color':'#F3F1F5',
-        'bg-1':'#f8f9fe',
+        'bg-1':'#fbf9fb',
+
         
        
       
@@ -28,7 +29,12 @@ module.exports = {
        fontFamily: {
         'sans': ['"Inter"', '"sans-serif"']
         
+       },
+       
+       boxShadow : {
+        'one': '0 1rem 1.5rem rgba(0,0,0,0.05)'
        }
+
     },
   },
   variants: {
